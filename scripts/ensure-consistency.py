d9d87d33713e2be1195c4fad6c6e3d7934d4ce62
Uri Laserson <uri.laserson@gmail.com>
2017-10-08 23:18:04 -0400

    Improve error messages in ensure-consistency.py

--- scripts/ensure-consistency.py
+++ scripts/ensure-consistency.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python
 
+import sys
 from glob import glob
 import os.path as osp
 
 import yaml
 import jinja2
+import jsondiff
 
 
 # function to extract name from file
@@ -18,8 +20,18 @@ py_files = {basename(f): f for f in glob('lang/python/airr/specs/*.yaml')}
 doc_files = {basename(f): f for f in glob('docs/*.md')}
 
 
-assert set(spec_files.keys()) == set(py_files.keys())
-assert len(set(spec_files.keys()) - set(doc_files.keys())) == 0
+if set(spec_files.keys()) != set(py_files.keys()):
+    for spec in set(spec_files.keys()) - set(py_files.keys()):
+        print(f'{spec} missing from python package', file=sys.stderr)
+    for spec in set(py_files.keys()) - set(spec_files.keys()):
+        print(f'{spec} found in python package but missing from specs/', file=sys.stderr)
+    sys.exit(1)
+
+
+if len(set(spec_files.keys()) - set(doc_files.keys())) > 0:
+    for spec in set(spec_files.keys()) - set(doc_files.keys()):
+        print(f'{spec} missing from rendered doc files', file=sys.stderr)
+    sys.exit(1)
 
 
 for spec_name in spec_files:
@@ -29,7 +41,10 @@ for spec_name in spec_files:
     with open(py_files[spec_name], 'r') as ip:
         py_spec = yaml.load(ip)
 
-    assert gold_spec == py_spec
+    if jsondiff.diff(gold_spec, py_spec) != {}:
+        print(f'{spec_name} spec is different from python version', file=sys.stderr)
+        print(jsondiff.diff(gold_spec, py_spec), file=sys.stderr)
+        sys.exit(1)
 
     # check that docs have been rendered
     with open(jinja_templates[spec_name], 'r') as ip:
@@ -37,4 +52,6 @@ for spec_name in spec_files:
     with open(doc_files[spec_name], 'r') as ip:
         doc_contents = ip.read().strip()
 
-    assert template.render(gold_spec) == doc_contents
+    if template.render(gold_spec) != doc_contents:
+        print(f'rendered doc for {spec_name} does not correspond to spec')
+        sys.exit(1)
