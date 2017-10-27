4802c40748bb38abb2066a76584ff11f7e4ddbd5
Scott Christley <scott.christley@utsouthwestern.edu>
2017-10-27 12:49:25 -0500

    add more consistency checks

--- scripts/check-consistency.py
+++ scripts/check-consistency.py
@@ -4,7 +4,21 @@ import sys
 from collections import Counter
 
 import yaml
+import csv
+from deepdiff import DeepDiff
 
+object_map = { '1 / study': 'MiAIRR_Study',
+               '1 / subject': 'MiAIRR_Subject',
+               '1 / diag. & intervent.': 'MiAIRR_Diagnosis',
+               '2 / sample': 'MiAIRR_Sample',
+               '3 / process (cell)': 'MiAIRR_CellProcessing',
+               '3 / process (nucl. acid)': 'MiAIRR_NucleicAcidProcessing',
+               '5 / process (comput.)': 'MiAIRR_SoftwareProcessing',
+               '6 / data (proc. seq.)': 'MiAIRR_Rearrangement' }
+
+with open('AIRR_Minimal_Standard_Data_Elements.tsv', 'r') as ip:
+    dictReader = csv.DictReader(ip, dialect='excel-tab')
+    miairr_elements = [line for line in dictReader]
 
 with open('AIRR_Minimal_Standard_Data_Elements.tsv', 'r') as ip:
     # header line present
@@ -15,31 +29,61 @@ with open('AIRR_Minimal_Standard_Data_Elements.tsv', 'r') as ip:
     assert table.count('') == 1
     _ = table.pop(table.index(''))
 
-
 with open('specs/definitions.yaml', 'r') as ip:
     definitions = yaml.load(ip)
     properties = [property
                   for obj in definitions.values()
-                  for property in obj['properties']]
+                  for property in obj['properties']
+                  if obj.get('discriminator') == 'MiAIRR']
 
+failed = False
 
 # check for uniqueness of fields in AIRR_Minimal_Standard_Data_Elements.tsv
 if len(table) != len(set(table)):
     for k, v in Counter(table).items():
         if v > 1:
             print(f'{k:30} found {v} times in tsv when it should be unique\n', file=sys.stderr)
-    sys.exit(1)
-
+    failed = True
 
 # check for differences in fields between specs/definitions.yaml and
 # AIRR_Minimal_Standard_Data_Elements.tsv
-if set(properties) != set(table):
-    for field in set(properties) - set(table):
-        print(f'{field:30} is found in yaml but not tsv', file=sys.stderr)
-    for field in set(table) - set(properties):
-        print(f'{field:30} is found in tsv but not yaml', file=sys.stderr)
-    sys.exit(1)
+for key in object_map.keys():
+    elements = [element['AIRR Formats WG field name'] for element in miairr_elements
+                if element['MiAIRR data set / subset'] == key]
+    definition = definitions.get(object_map[key])
+    if not definition:
+        print(f'{object_map[key]} not found in definitions.yaml.\n', file=sys.stderr)
+        failed = True
+        continue
 
+    properties = [property for property in definition['properties']]
+    if set(elements) != set(properties):
+        for field in set(properties) - set(elements):
+            print(f'{field:30} is found in yaml but not tsv for {object_map[key]}', file=sys.stderr)
+        for field in set(elements) - set(properties):
+            print(f'{field:30} is found in tsv but not yaml for {object_map[key]}', file=sys.stderr)
+        failed = True
+
+# check that MiAIRR object definitions contained
+# within AIRR definition
+for definition in definitions.keys():
+    if definitions[definition].get('discriminator') == 'MiAIRR':
+        name = definition.split('_')[1]
+        if not definitions.get(name):
+            print(f'{name} corresponding to {definition} not found in definitions.yaml', file=sys.stderr)
+            failed = True
+            continue
+
+        for prop in definitions[definition]['properties']:
+            if not definitions[name]['properties'].get(prop):
+                print(f'{prop} in {definition} object is not in {name} object.', file=sys.stderr)
+                failed = True
+                continue
+            ddiff = DeepDiff(definitions[definition]['properties'][prop], definitions[name]['properties'][prop], ignore_order=True)
+            if ddiff:
+                print(f'{prop} in {definition} object is not the same object in {name}.', file=sys.stderr)
+                print(ddiff, file=sys.stderr)
+                failed = True
 
 # check consistency with NCBI XML definitions, per @BusseChristian's pseudocode
 # in https://github.com/airr-community/airr-standards/issues/20
@@ -57,4 +101,6 @@ if miairr_identifiers != ncbi_identifiers:
         print(f'{field:30} is found in MiAIRR table tsv but not in NCBI Biosample template xls', file=sys.stderr)
     for field in set(ncbi_identifiers) - set(miairr_identifiers):
         print(f'{field:30} is found in NCBI Biosample template xls but not in MiAIRR table tsv', file=sys.stderr)
-    sys.exit(1)
+    failed = True
+
+if failed: sys.exit(1)
