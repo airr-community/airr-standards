643d0f837d356414475ff98fcfe76d3fdb90c0df
Uri Laserson <uri.laserson@gmail.com>
2017-11-05 16:49:56 -0500

    Refactor consistency check
    Fixes #51

--- scripts/check-consistency.py
+++ scripts/check-consistency.py
@@ -1,110 +1,103 @@
 #! /usr/bin/env python
 
 import sys
+import csv
 from collections import Counter
 
 import yaml
-import csv
+import pandas as pd
 from deepdiff import DeepDiff
 
-object_map = { '1 / study': 'MiAIRR_Study',
-               '1 / subject': 'MiAIRR_Subject',
-               '1 / diag. & intervent.': 'MiAIRR_Diagnosis',
-               '2 / sample': 'MiAIRR_Sample',
-               '3 / process (cell)': 'MiAIRR_CellProcessing',
-               '3 / process (nucl. acid)': 'MiAIRR_NucleicAcidProcessing',
-               '5 / process (comput.)': 'MiAIRR_SoftwareProcessing',
-               '6 / data (proc. seq.)': 'MiAIRR_Rearrangement' }
+
+miairr_dataset_to_api_object = {
+    '1 / study': 'MiAIRR_Study',
+    '1 / subject': 'MiAIRR_Subject',
+    '1 / diag. & intervent.': 'MiAIRR_Diagnosis',
+    '2 / sample': 'MiAIRR_Sample',
+    '3 / process (cell)': 'MiAIRR_CellProcessing',
+    '3 / process (nucl. acid)': 'MiAIRR_NucleicAcidProcessing',
+    '5 / process (comput.)': 'MiAIRR_SoftwareProcessing',
+    '6 / data (proc. seq.)': 'MiAIRR_Rearrangement'}
+
 
 with open('AIRR_Minimal_Standard_Data_Elements.tsv', 'r') as ip:
-    dictReader = csv.DictReader(ip, dialect='excel-tab')
-    miairr_elements = [line for line in dictReader]
+    tsv_data = list(csv.DictReader(ip, dialect='excel-tab'))
 
-with open('AIRR_Minimal_Standard_Data_Elements.tsv', 'r') as ip:
-    # header line present
-    assert next(ip).split()[0] == 'MiAIRR'
-
-    table = [line.split('\t')[6].strip() for line in ip]
-    # handle the exceptional 4 / data line
-    assert table.count('') == 1
-    _ = table.pop(table.index(''))
 
 with open('specs/definitions.yaml', 'r') as ip:
-    definitions = yaml.load(ip)
-    properties = [property
-                  for obj in definitions.values()
-                  for property in obj['properties']
-                  if obj.get('discriminator') == 'MiAIRR']
+    yaml_data = yaml.load(ip)
+
 
 failed = False
 
+
 # check for uniqueness of fields in AIRR_Minimal_Standard_Data_Elements.tsv
-if len(table) != len(set(table)):
+fields = [e['AIRR Formats WG field name'] for e in tsv_data]
+if len(fields) != len(set(fields)):
     print('Duplicate entries found in AIRR_Minimal_Standard_Data_Elements.tsv', file=sys.stderr)
-    for k, v in Counter(table).items():
+    for k, v in Counter(fields).items():
         if v > 1:
             print(f'{k:30} found {v} times in tsv when it should be unique\n', file=sys.stderr)
     failed = True
 
+
 # check for differences in fields between specs/definitions.yaml and
 # AIRR_Minimal_Standard_Data_Elements.tsv
-for key in object_map.keys():
-    elements = [element['AIRR Formats WG field name'] for element in miairr_elements
-                if element['MiAIRR data set / subset'] == key]
-    definition = definitions.get(object_map[key])
-    if not definition:
-        print(f'{object_map[key]} not found in definitions.yaml.\n', file=sys.stderr)
+for dataset in miairr_dataset_to_api_object.keys():
+    api_object = miairr_dataset_to_api_object[dataset]
+    tsv_fields = [row['AIRR Formats WG field name']
+                  for row in tsv_data
+                  if row['MiAIRR data set / subset'] == dataset]
+    yaml_object = yaml_data.get(api_object, None)
+    if not yaml_object:
+        print(f'{api_object} not found in definitions.yaml.\n', file=sys.stderr)
         failed = True
         continue
-
-    properties = [property for property in definition['properties']]
-    if set(elements) != set(properties):
-        print(f'{object_map[key]} does not match TSV', file=sys.stderr)
-        for field in set(properties) - set(elements):
-            print(f'{field:30} is found in yaml but not tsv for {object_map[key]}', file=sys.stderr)
-        for field in set(elements) - set(properties):
-            print(f'{field:30} is found in tsv but not yaml for {object_map[key]}', file=sys.stderr)
+    yaml_fields = [property for property in yaml_object['properties']]
+    if set(tsv_fields) != set(yaml_fields):
+        print(f'yaml {api_object} does not match tsv {dataset}', file=sys.stderr)
+        for field in set(yaml_fields) - set(tsv_fields):
+            print(f'{field:30} is found in yaml {api_object} but not tsv {dataset}', file=sys.stderr)
+        for field in set(tsv_fields) - set(yaml_fields):
+            print(f'{field:30} is found in tsv {dataset} but not yaml {api_object}', file=sys.stderr)
         failed = True
+
 
 # check that MiAIRR object definitions contained
 # within AIRR definition
-for definition in definitions.keys():
-    if definitions[definition].get('discriminator') == 'MiAIRR':
-        name = definition.split('_')[1]
-        if not definitions.get(name):
-            print(f'{name} corresponding to {definition} not found in definitions.yaml', file=sys.stderr)
+for miairr_api_object in yaml_data.keys():
+    if yaml_data[miairr_api_object]['discriminator'] == 'MiAIRR':
+        airr_api_object = miairr_api_object.split('_')[1]
+        if airr_api_object not in yaml_data:
+            print(f'{airr_api_object} corresponding to {miairr_api_object} not found in definitions.yaml', file=sys.stderr)
             failed = True
             continue
 
-        for prop in definitions[definition]['properties']:
-            if not definitions[name]['properties'].get(prop):
-                print(f'{prop} in {definition} object is not in {name} object.', file=sys.stderr)
+        for miairr_field in yaml_data[miairr_api_object]['properties']:
+            if miairr_field not in yaml_data[airr_api_object]['properties']:
+                print(f'{miairr_field} in {miairr_api_object} object is not in {airr_api_object} object.', file=sys.stderr)
                 failed = True
                 continue
-            ddiff = DeepDiff(definitions[definition]['properties'][prop], definitions[name]['properties'][prop], ignore_order=True)
+            ddiff = DeepDiff(yaml_data[miairr_api_object]['properties'][miairr_field], yaml_data[airr_api_object]['properties'][miairr_field], ignore_order=True)
             if ddiff:
-                print(f'{prop} in {definition} object is not the same object in {name}.', file=sys.stderr)
+                print(f'{miairr_field} in {miairr_api_object} object is not the same object in {airr_api_object}.', file=sys.stderr)
                 print(ddiff, file=sys.stderr)
                 failed = True
 
+
 # check consistency with NCBI XML definitions, per @BusseChristian's pseudocode
 # in https://github.com/airr-community/airr-standards/issues/20
-import pandas as pd
-
-miairr_table = pd.read_csv('AIRR_Minimal_Standard_Data_Elements.tsv', sep='\t', header=0, index_col=None)
-miairr_biosample_rows = miairr_table.iloc[:, 0].isin(["1 / subject", "1 / diag. & intervent.", "2 / sample", "3 / process (cell)"])
-miairr_identifiers = set(miairr_table[miairr_biosample_rows].iloc[:, 6])
-miairr_identifiers.add('study_id') # manually add
-miairr_mapping = {}
 with open('NCBI_implementation/mapping_MiAIRR_BioSample.tsv', 'r') as ip:
-    dictReader = csv.DictReader(ip, dialect='excel-tab')
-    for line in dictReader:
-        miairr_mapping[line['AIRR Formats WG field name']] = line['NCBI BioSample attribute']
-mapped_identifiers = set([miairr_mapping.get(name, name) for name in miairr_identifiers])
-
-ncbi_biosample = pd.read_excel('NCBI_implementation/templates_XLS/AIRR_BioSample_v1.0.xls', skiprows=13)
-ncbi_identifiers = set([x.lstrip('*') for x in ncbi_biosample.columns])
-
+    miairr_to_ncbi = {r['AIRR Formats WG field name']: r['NCBI BioSample attribute']
+                      for r in csv.DictReader(ip, dialect='excel-tab')}
+miairr_biosample_identifiers = [r['AIRR Formats WG field name']
+                                for r in tsv_data
+                                if r['MiAIRR data set / subset'] in ["1 / subject", "1 / diag. & intervent.", "2 / sample", "3 / process (cell)"]]
+miairr_biosample_identifiers.append('study_id') # manually add
+mapped_identifiers = {miairr_to_ncbi.get(name, name)
+                      for name in miairr_biosample_identifiers}
+ncbi_biosample_template = pd.read_excel('NCBI_implementation/templates_XLS/AIRR_BioSample_v1.0.xls', skiprows=13)
+ncbi_identifiers = set([x.lstrip('*') for x in ncbi_biosample_template.columns])
 if mapped_identifiers != ncbi_identifiers:
     print('AIRR_Minimal_Standard_Data_Elements.tsv does not match AIRR_BioSample_v1.0.xls', file=sys.stderr)
     for field in set(mapped_identifiers) - set(ncbi_identifiers):
@@ -112,6 +105,7 @@ if mapped_identifiers != ncbi_identifiers:
     for field in set(ncbi_identifiers) - set(mapped_identifiers):
         print(f'{field:30} is found in NCBI Biosample template xls but not in MiAIRR table tsv', file=sys.stderr)
     failed = True
+
 
 if failed:
     print('consistency checks failed', file=sys.stderr)
