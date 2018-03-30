83f709a04b1d94483873b8ad3fdec6c34b43ae25
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2018-03-30 14:05:55 -0400

    R package (#102)

---
+++ lang/R/R/AIRR.R
@@ -0,0 +1,11 @@
+#' airr
+#' 
+#' AIRR API Package
+#' 
+#' @name        airr
+#' 
+#' @importFrom  readr    read_tsv write_tsv 
+#'                       cols col_character col_logical col_integer col_double
+#' @importFrom  stats    setNames
+#' @importFrom  stringi  stri_trim
+NULL
