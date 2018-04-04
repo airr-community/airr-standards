ab27c435529d95f78389f4e136778ae1abbb8d16
ssnn-airr <20683719+ssnn-airr@users.noreply.github.com>
2018-04-04 19:01:50 -0400

    Life happens. Back to 24fb642 + date change

--- lang/R/R/Interface.R
+++ lang/R/R/Interface.R
@@ -4,8 +4,12 @@
 #' 
 #' \code{read_airr} reads a TSV container AIRR Rearrangement records.
 #'
-#' @param    file        input file path.
-#' @param    positions   if \code{TRUE} modify positional fields to 1-based indexes.
+#' @param    file    input file path.
+#' @param    base    starting index for positional fields in the input file. 
+#'                   If \code{0}, then fields ending in \code{_start} and \code{_end}
+#'                   are 0-based half-open intervals (python style) in the input file 
+#'                   and will be converted to 1-based closed-intervals (R style). 
+#'                   If \code{1}, then these fields will not be modified.
 #' 
 #' @return   A data.frame of the TSV file with appropriate type and position conversion
 #'           for fields defined in the specification.
@@ -19,26 +23,29 @@
 #' }
 #' 
 #' @export
-read_airr <- function(file, positions=TRUE) {
-   # Define types
-   parsers <- c("character"="c", "logical"="l", "integer"="i", "double"="d")
-   header <- names(suppressMessages(readr::read_tsv(file, n_max=1)))
-   schema_fields <- intersect(names(RearrangementSchema), header)
-   cast <- setNames(lapply(schema_fields, function(f) parsers[RearrangementSchema[f]$type]), schema_fields)
-   types <- do.call(readr::cols, cast)
-   
-   # Read file    
-   data <- suppressMessages(readr::read_tsv(file, col_types=types, na=c("", "NA", "None")))
-   
-   # Adjust indexes
-   if (positions) {
-      start_positions <- grep("_start$", names(data), perl=TRUE)
-      if (length(start_positions) > 0) {
-         data[, start_positions] <- data[, start_positions] + 1
-      }
-   }
-   
-   return(data)
+read_airr <- function(file, base=c(0, 1)) {
+    # Check arguments
+    base <- match.arg(base)
+    
+    # Define types
+    parsers <- c("character"="c", "logical"="l", "integer"="i", "double"="d")
+    header <- names(suppressMessages(readr::read_tsv(file, n_max=1)))
+    schema_fields <- intersect(names(RearrangementSchema), header)
+    cast <- setNames(lapply(schema_fields, function(f) parsers[RearrangementSchema[f]$type]), schema_fields)
+    types <- do.call(readr::cols, cast)
+    
+    # Read file    
+    data <- suppressMessages(readr::read_tsv(file, col_types=types, na=c("", "NA", "None")))
+    
+    # Adjust indexes
+    if (base == 0) {
+        start_positions <- grep("_start$", names(data), perl=TRUE)
+        if (length(start_positions) > 0) {
+            data[, start_positions] <- data[, start_positions] + 1
+        }
+    }
+
+    return(data)
 }
 
 
@@ -46,9 +53,14 @@ read_airr <- function(file, positions=TRUE) {
 #' 
 #' \code{write_airr} writes an TSV container AIRR Rearrangement records.
 #'
-#' @param    data        data.frame of Rearrangement data.
-#' @param    file        output file name.
-#' @param    positions   if \code{TRUE} modify positional fields to 0-based indexes.
+#' @param    data    data.frame of Rearrangement data.
+#' @param    file    output file name.
+#' @param    base    starting index for positional fields in the output file. 
+#'                   If \code{0}, then fields ending in \code{_start} and \code{_end}
+#'                   will be converted to 0-based half-open intervals (python style) 
+#'                   in the output file. If \code{1}, then these fields will not be 
+#'                   modified. Fields in the input \code{data} are assumed to be 
+#'                   1-based closed-intervals (R style). 
 #'
 #' @return   NULL
 #' 
@@ -61,27 +73,30 @@ read_airr <- function(file, positions=TRUE) {
 #' }
 #' 
 #' @export
-write_airr <- function(data, file, positions=TRUE) {
-   ## DEBUG
-   # data <- data.frame("sequence_id"=1:4, "extra"=1:4, "a"=LETTERS[1:4])
-   
-   # Fill in missing required columns
-   missing <- setdiff(RearrangementSchema@required, names(data))
-   data[, missing] <- NA
-   
-   # order columns
-   ordering <- c(intersect(names(RearrangementSchema), names(data)),
-                 setdiff(names(data), names(RearrangementSchema)))
-   data <- data[, ordering]
-   
-   # Adjust indexes
-   if (positions) {
-      start_positions <- grep("_start$", names(data), perl=TRUE)
-      if (length(start_positions) > 0) {
-         data[, start_positions] <- data[, start_positions] + 1
-      }
-   }
-   
-   # Write
-   write_tsv(data, file, na="")
+write_airr <- function(data, file, base=c(0, 1)) {
+    ## DEBUG
+    # data <- data.frame("sequence_id"=1:4, "extra"=1:4, "a"=LETTERS[1:4])
+
+    # Check arguments
+    base <- match.arg(base)
+    
+    # Fill in missing required columns
+    missing <- setdiff(RearrangementSchema@required, names(data))
+    data[, missing] <- NA
+    
+    # order columns
+    ordering <- c(intersect(names(RearrangementSchema), names(data)),
+                  setdiff(names(data), names(RearrangementSchema)))
+    data <- data[, ordering]
+    
+    # Adjust indexes
+    if (base == 0) {
+        start_positions <- grep("_start$", names(data), perl=TRUE)
+        if (length(start_positions) > 0) {
+            data[, start_positions] <- data[, start_positions] + 1
+        }
+    }
+    
+    # Write
+    write_tsv(data, file, na="")
 }
--- lang/R/R/Schema.R
+++ lang/R/R/Schema.R
@@ -1,24 +1,3 @@
-#### Data ####
-
-#' Alignment Schema
-#'
-#' AIRR Alignment object schema
-#'
-#' @format   A \link{Schema} object.
-#' 
-#' @export
-AlignmentSchema <- load_schema("Alignment")
-
-#' Rearrangment Schema
-#'
-#' AIRR Rearrangement object schema
-#'
-#' @format   A \link{Schema} object.
-#' 
-#' @seealso See \link{read_airr} and \link{write_airr} for reading and writing.
-"RearrangementSchema"
-
-
 #### Classes ####
 
 #' S4 class defining an AIRR standard schema
@@ -71,19 +50,33 @@ setMethod("$",
           function (x, name) { x@properties[[name]] })
 
 
-#### Schema Functions ####
+#### Schema I/O ####
 
-#' Read an AIRR Rearrangement TSV
+#' Load a schema definition
 #' 
-#' \code{read_airr} reads a TSV container AIRR Rearrangement records.
+#' \code{load_schema} loads an AIRR object definition from the internal
+#' definition set.
 #'
-#' @param    file        input file path.
-#' @param    positions   if \code{TRUE} modify positional fields to 1-based indexes.
+#' @param    definition   name of the schema definition.
 #' 
-#' @return   A data.frame of the TSV file with appropriate type and position conversion
-#'           for fields defined in the specification.
-#'                   
-#' @seealso  See \link{write_airr} for writing to AIRR Rearrangement data.
+#' @return   A \link{Schema} object for the \code{definition}.
+#' 
+#' @details
+#' Valid definitions include:
+#' \itemize{
+#'   \item   \code{"Rearrangement"}
+#'   \item   \code{"Alignment"}
+#'   \item   \code{"Study"}
+#'   \item   \code{"Subject"}
+#'   \item   \code{"Diagnosis"}
+#'   \item   \code{"Sample"}
+#'   \item   \code{"CellProcessing"}
+#'   \item   \code{"NucleicAcidProcessing"}
+#'   \item   \code{"RawSequenceData"}
+#'   \item   \code{"SoftwareProcessing"}
+#' }
+#'    
+#' @seealso  See \link{Schema} for the return object.
 #' 
 #' @examples
 #' # Load the rearrangement definition
@@ -91,28 +84,51 @@ setMethod("$",
 #' 
 #' @export
 load_schema <- function(definition) {
-   # Load schema from yaml file
-   spec_file <- system.file("extdata", "definitions.yaml", package="airr")
-   spec_list <- yaml::yaml.load_file(spec_file)
-   
-   # Load definition
-   definition_list <- spec_list[[definition]]
-   definition_list[["discriminator"]] <- definition_list[["type"]] <- NULL
-   
-   # Define member attributes
-   fields <- names(definition_list[["properties"]])
-   properties <- definition_list[["properties"]]
-   required <- definition_list[["required"]]
-   optional <- setdiff(fields, required)
-   
-   # Rename type and clean description
-   types <- c("string"="character", "boolean"="logical", "integer"="integer", "number"="double")
-   for (f in fields) {
-      x <- properties[[f]][["type"]]
-      y <- properties[[f]][["description"]]
-      properties[[f]][["type"]] <- unname(types[x])
-      properties[[f]][["description"]] <- stri_trim(y)
-   }
-   
-   return(new("Schema", required=required, optional=optional, properties=properties))
+    # Load schema from yaml file
+    spec_file <- system.file("extdata", "definitions.yaml", package="airr")
+    spec_list <- yaml::yaml.load_file(spec_file)
+    
+    # Load definition
+    definition_list <- spec_list[[definition]]
+    definition_list[["discriminator"]] <- definition_list[["type"]] <- NULL
+    
+    # Define member attributes
+    fields <- names(definition_list[["properties"]])
+    properties <- definition_list[["properties"]]
+    required <- definition_list[["required"]]
+    optional <- setdiff(fields, required)
+    
+    # Rename type and clean description
+    types <- c("string"="character", "boolean"="logical", "integer"="integer", "number"="double")
+    for (f in fields) {
+        x <- properties[[f]][["type"]]
+        y <- properties[[f]][["description"]]
+        properties[[f]][["type"]] <- unname(types[x])
+        properties[[f]][["description"]] <- stri_trim(y)
+    }
+    
+    return(new("Schema", required=required, optional=optional, properties=properties))
 }
+
+
+#### Data ####
+
+#' Alignment Schema
+#'
+#' AIRR Alignment object schema
+#'
+#' @format   A \link{Schema} object.
+#' 
+#' @export
+AlignmentSchema <- load_schema("Alignment")
+
+#' Rearrangment Schema
+#'
+#' AIRR Rearrangement object schema
+#'
+#' @format   A \link{Schema} object.
+#' 
+#' @seealso See \link{read_airr} and \link{write_airr} for reading and writing.
+#' 
+#' @export
+RearrangementSchema <- load_schema("Rearrangement")
