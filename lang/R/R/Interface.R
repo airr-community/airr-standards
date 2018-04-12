ff9b66a3f5d34de73f9a9ee3d73df3b1736700c4
ssnn-airr <20683719+ssnn-airr@users.noreply.github.com>
2018-04-12 11:11:29 -0400

    Oops. Changed the +1 to -1 in the wrong function...

--- lang/R/R/Interface.R
+++ lang/R/R/Interface.R
@@ -42,7 +42,7 @@ read_airr <- function(file, base=0 ) {
     if (base == 0) {
         start_positions <- grep("_start$", names(data), perl=TRUE)
         if (length(start_positions) > 0) {
-            data[, start_positions] <- data[, start_positions] - 1
+            data[, start_positions] <- data[, start_positions] + 1
         }
     }
 
@@ -95,7 +95,7 @@ write_airr <- function(data, file, base=0) {
     if (base == 0) {
         start_positions <- grep("_start$", names(data), perl=TRUE)
         if (length(start_positions) > 0) {
-            data[, start_positions] <- data[, start_positions] + 1
+            data[, start_positions] <- data[, start_positions] - 1
         }
     }
     
