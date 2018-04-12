ed494a125103356c302425672f749853ebf1b411
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2018-04-12 15:14:11 -0400

    Merging R package so I can delete the branches (#109)
    * Start of R package.
    
    * Refactor schema
    1. Modified schema definitions to mirror python API structure.
    2. Added schema loader function.
    3. Added unit test skeleton.
    4. Moved definitions.yaml into inst/extdata.
    
    * Minor.
    
    * Schema updates
    
    1. Moved predefined schema object some `/data` to globals loaded from the definitions.yaml on import. Which is bad R behavior, but should be easier to maintain.
    2. Doc fixes and improvements.
    3. Renamed indexing arguments in `read_airr` and `write_airr`.
    
    * Fixes to pass `R CMD check --as-cran`.
    
    * Updated ignore rule for R vignettes
    
    * ignore not Rmd files in vignettes
    
    * Added vignette skeleton
    
    * Updated from mandatory to required to match current spec
    
    * Updated date
    
    * Merge conflict fixes
    
    * Oops
    
    * Back to master NAMESPACE
    
    * Revert "Updated date"
    
    This reverts commit 1b27db311fcda35f2200ce76cae780c2cb56de45.
    
    * Date
    
    * Added back export
    
    * Life happens. Back to 24fb642 + date change
    
    * IgBLAST and R package updates (#103)
    
    1. Added individual segment alignment fields for IgBLAST support and other local segment alignment tools.
    2. Added R package to `definitions.yaml` consistency checks.
    
    * Renamed `mandatory` to `required` in `airr.Schema.Schema` (python package).
    
    * testthat recognizes files with name starting in test
    
    * match.arg was not working with default vector of numeric values
    
    * test read_airr loads a data.frame
    
    * write_airr test expects test_out.tsv
    
    * Minor. as.numeric not needed, added test
    
    * Should be -1 to go back to 0 based
    
    * Oops. Changed the +1 to -1 in the wrong function...
    
    * Remove old MiAIRR-to-NCBI specification
    
    Content has been transferred to RST system in commit 0b14b6b.
    
    * Updated doc requirements
    
    1. Updated from sphinxcontrib.napoleon to sphinx.ext.napoleon.
    2. Added sphinx>=1.4 requirement.
    3. Added sphinxcontrib-autoprogram dependencies, because we'll need it soon.
    
    * Move content of NCBI_implementation/README.md to Sphinx documentation
    
    - move linked images into docs/ tree
    - minor restructuring of MiAIRR documentation files
    
    * Re-format NCBI_implementation/mapping_README to RST
    
    * Reformat and update main README to RST
    
    * Reformat CONTRIBUTING to RST, fix typos and links
    
    * Typo fix.
    
    * Typo fix.
    
    * Oops. Was fine before.
    
    * Attempt to fix merge issues.
    
    * R package (#104)
    
    R package updates.
    
    1. Modified schema definitions to mirror python API structure.
    2. Added schema loader function.
    3. Added unit test skeleton.
    4. Moved definitions.yaml into inst/extdata.
    5. Moved predefined schema object some `/data` to globals loaded from the definitions.yaml on import. Which is bad R behavior, but should be easier to maintain.
    6. Doc fixes and improvements.
    7. Renamed indexing arguments in `read_airr` and `write_airr`.
    8. Fixes to pass `R CMD check --as-cran`.
    9. Updated ignore rule for R vignettes
    10. Added vignette skeleton
    11. Updated from mandatory to required to match current spec
    12. Updated date

---
+++ lang/R/tests/testthat/testInterface.R
@@ -0,0 +1,23 @@
+# Toy file pointer
+rearrangement_file <- file.path("..", "data-tests", "toy_data.tsv")
+#rearrangement_file <- file.path("tests", "data-tests", "toy_data.tsv")
+
+#### Rearrangement I/O  ####
+
+test_that("read_airr", {
+    tbl_0 <- read_airr(rearrangement_file, 0)
+    expect_true(is.data.frame(tbl_0))
+    
+    tbl_1 <- read_airr(rearrangement_file, 1)
+    expect_true(is.data.frame(tbl_1))
+    
+    start_positions <- grep("_start$", names(tbl_0), perl=TRUE)
+    
+    expect_equivalent(tbl_0[,start_positions]-1, tbl_1[,start_positions])
+})
+
+test_that("write_airr", {
+    tbl <- read_airr(rearrangement_file)
+    write_airr(tbl, "test_out.tsv")
+    expect_true(file.exists("test_out.tsv"))
+})
