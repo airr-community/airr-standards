c05bacb1e1f28d455c8ecf007b68d29d98d792a9
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2018-04-17 13:08:05 -0400

    Abstract read/write to different schemas in R package. (#119)
    Abstract read/write to different schemas in R package.
    
    1. Generalized `read_airr` and `write_airr` to any schema
    2. Added simple `read_rearrangement`, `read_alignment`, `write_rearrangement`, `write_alignment` wrappers.
    3. Switch type of `base` argument to string.
    4. Added NEWS template.
    5. Added ellipses arguments.

--- lang/R/tests/testthat/test-Interface.R
+++ lang/R/tests/testthat/test-Interface.R
@@ -5,15 +5,15 @@ rearrangement_file <- file.path("..", "data-tests", "toy_data.tsv")
 #### Rearrangement I/O  ####
 
 test_that("read_airr", {
-    tbl_0 <- read_airr(rearrangement_file, 0)
+    tbl_0 <- read_airr(rearrangement_file, "0")
     expect_true(is.data.frame(tbl_0))
     
-    tbl_1 <- read_airr(rearrangement_file, 1)
+    tbl_1 <- read_airr(rearrangement_file, "1")
     expect_true(is.data.frame(tbl_1))
     
     start_positions <- grep("_start$", names(tbl_0), perl=TRUE)
     
-    expect_equivalent(tbl_0[,start_positions]-1, tbl_1[,start_positions])
+    expect_equivalent(tbl_0[, start_positions] - 1, tbl_1[, start_positions])
 })
 
 test_that("write_airr", {
