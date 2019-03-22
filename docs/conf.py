84479c15ddc8b65ca9d744b5904f0cb9f402e357
Scott Christley <scott.christley@utsouthwestern.edu>
2019-03-22 14:38:56 -0500

    increase width slightly

--- docs/conf.py
+++ docs/conf.py
@@ -110,6 +110,7 @@ html_theme_options = {'github_user': 'airr-community',
                       'github_repo': 'airr-standards',
                       'github_button': True,
                       'sidebar_includehidden': True,
+                      'sidebar_width': '300px',
                       'extra_nav_links': {'AIRR Community': 'http://airr-community.org'}}
 html_sidebars = {'**': ['about.html',
                         'navigation.html',
