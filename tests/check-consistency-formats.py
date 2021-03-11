b0ac30012586f6afeccc78b373cef70613c4ff76
Francisco Arcila <f.arcila@dkfz-heidelberg.de>
2021-03-11 13:58:17 +0100

    remove dict unpacking for backwards compatibility

--- tests/check-consistency-formats.py
+++ tests/check-consistency-formats.py
@@ -11,21 +11,32 @@ from glob import glob
 basename = lambda f: os.path.splitext(os.path.basename(f))[0]
 
 # load all paths keyed by their name
-test_files = {**{"python": {basename(f): f for f in glob('lang/python/tests/data/*')}},
-              **{"R": {basename(f): f for f in glob('lang/R/tests/data-tests/*')}}}
-spec_files = {**{"spec": {basename(f): f for f in glob('specs/airr-schema.yaml')}},
-              **{"python": {basename(f): f for f in glob('lang/python/airr/specs/airr-schema.yaml')}},
-              **{"R": {basename(f): f for f in glob('lang/R/inst/extdata/airr-schema.yaml')}}
-              }
+# to be uncommented when we drop python 2 support
+# test_files = {**{"python": {basename(f): f for f in glob('lang/python/tests/data/*')}},
+#               **{"R": {basename(f): f for f in glob('lang/R/tests/data-tests/*')}}}
+# spec_files = {**{"spec": {basename(f): f for f in glob('specs/airr-schema.yaml')}},
+#               **{"python": {basename(f): f for f in glob('lang/python/airr/specs/airr-schema.yaml')}},
+#               **{"R": {basename(f): f for f in glob('lang/R/inst/extdata/airr-schema.yaml')}}
+#               }
 
+def merge_two_dicts(x, y): # remove when python 2 support is dropped and use code from above for 3.5+
+    z = x.copy()
+    z.update(y)
+    return z
 
-def check_file_sync(dic_files: dict, files_key_a: str, files_key_b: str):
+test_files = merge_two_dicts({"python": {basename(f): f for f in glob('lang/python/tests/data/*')}},
+              {"R": {basename(f): f for f in glob('lang/R/tests/data-tests/*')}})
+spec_files = merge_two_dicts((merge_two_dicts({"spec": {basename(f): f for f in glob('specs/airr-schema.yaml')}},
+              {"python": {basename(f): f for f in glob('lang/python/airr/specs/airr-schema.yaml')}})),
+            {"R": {basename(f): f for f in glob('lang/R/inst/extdata/airr-schema.yaml')}})
+
+def check_file_sync(dic_files, files_key_a, files_key_b):
     # Check python package specs
     if set(dic_files[files_key_a].keys()) != set(dic_files[files_key_b].keys()):
         for spec in set(dic_files[files_key_a].keys()) - set(dic_files[files_key_b].keys()):
             print('{} missing from {} package'.format(spec, files_key_b, file=sys.stderr))
         for spec in set(dic_files[files_key_b].keys()) - set(dic_files[files_key_a].keys()):
-            print('{} found in {} package but missing from {}/'.format(spec, files_key_b, files_key_a), file=sys.stderr)
+            print('{} found in {} package but missing from {}/'.format(spec, files_key_b, files_key_a))
         sys.exit(1)
 
 
@@ -40,16 +51,16 @@ check_file_sync(test_files, "R", "python")
 for spec_name in spec_files["spec"]:
 
     if not filecmp.cmp(spec_files["spec"][spec_name], spec_files["python"][spec_name]):
-        print('{} spec is different from python version'.format(spec_name), file=sys.stderr)
+        print('{} spec is different from python version'.format(spec_name))
         sys.exit(1)
     if not filecmp.cmp(spec_files["spec"][spec_name], spec_files["R"][spec_name]):
-        print('{} spec is different from R version'.format(spec_name), file=sys.stderr)
+        print('{} spec is different from R version'.format(spec_name))
         sys.exit(1)
 
 # check equality of test files
 for file_name in test_files["R"]:
 
     if not filecmp.cmp(test_files["R"][file_name], test_files["python"][file_name]):
-        print('R {} test file is different from python version'.format(file_name), file=sys.stderr)
+        print('R {} test file is different from python version'.format(file_name))
         sys.exit(1)
 
