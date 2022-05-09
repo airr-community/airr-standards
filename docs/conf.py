43c2a01b83d4dea367b2941560d900c2a549d829
Christian Busse <christian.busse@dkfz-heidelberg.de>
2022-05-09 16:23:17 +0200

    Update Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -408,7 +408,7 @@ tables = ['Repertoire', 'Study', 'Subject', 'Diagnosis', 'Sample', 'CellProcessi
           'PCRTarget', 'SequencingRun', 'RawSequenceData', 'DataProcessing',
           'Rearrangement', 'Alignment', 'Clone', 'Tree', 'Node', 'Cell', 'CellExpression',
           'RearrangedSequence', 'UnrearrangedSequence', 'SequenceDelineationV', 'AlleleDescription', 'GermlineSet',
-          'GenotypeSet', 'Genotype', 'MHCGenotypeSet', 'MHCGenotype']
+          'GenotypeSet', 'Genotype', 'MHCGenotypeSet', 'MHCGenotype', 'Receptor']
 for spec in tables:
     with open(os.path.join(download_path, '%s.tsv' % spec), 'w') as f:
         writer = csv.DictWriter(f, fieldnames=fields, dialect='excel-tab', extrasaction='ignore')
