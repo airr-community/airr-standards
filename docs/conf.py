78866cd82c007c77f5f5f5f85c626ce95db48b59
Jason Vander Heiden <vandej27@gene.com>
2022-08-05 17:07:47 -0700

    Fix doc config

--- docs/conf.py
+++ docs/conf.py
@@ -405,7 +405,7 @@ html_context['MiAIRR_schema'] = miairr_schema
 # Write individual spec TSVs
 fields = ['Name', 'Type', 'Attributes', 'Definition']
 tables = ['Repertoire', 'Study', 'Subject', 'Diagnosis', 'Sample', 'CellProcessing', 'NucleicAcidProcessing',
-          'PCRTarget', 'SequencingRun', 'RawSequenceData', 'DataProcessing',
+          'PCRTarget', 'SequencingRun', 'SequencingData', 'DataProcessing',
           'Rearrangement', 'Alignment', 'Clone', 'Tree', 'Node', 'Cell', 'CellExpression',
           'RearrangedSequence', 'UnrearrangedSequence', 'SequenceDelineationV', 'AlleleDescription', 'GermlineSet',
           'GenotypeSet', 'Genotype', 'MHCGenotypeSet', 'MHCGenotype', 'Receptor']
