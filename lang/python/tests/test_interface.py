fea6431569b12a6e94fa78e802b8fec815dc1bf5
Jason Vander Heiden <vandej27@gene.com>
2022-08-06 19:50:30 -0700

    Add combined data and yaml vs json tests

--- lang/python/tests/test_interface.py
+++ lang/python/tests/test_interface.py
@@ -22,14 +22,16 @@ class TestInferface(unittest.TestCase):
         print('-------> %s()' % self.id())
 
         # Test data
-        self.data_good = os.path.join(data_path, 'good_rearrangement.tsv')
-        self.data_bad = os.path.join(data_path, 'bad_rearrangement.tsv')
+        self.rearrangement_good = os.path.join(data_path, 'good_rearrangement.tsv')
+        self.rearrangement_bad = os.path.join(data_path, 'bad_rearrangement.tsv')
         self.rep_good = os.path.join(data_path, 'good_repertoire.yaml')
         self.rep_bad = os.path.join(data_path, 'bad_repertoire.yaml')
         self.germline_good = os.path.join(data_path, 'good_germline_set.json')
         self.germline_bad = os.path.join(data_path, 'bad_germline_set.json')
         self.genotype_good = os.path.join(data_path, 'good_genotype_set.json')
         self.genotype_bad = os.path.join(data_path, 'bad_genotype_set.json')
+        self.combined_yaml = os.path.join(data_path, 'good_combined_airr.yaml')
+        self.combined_json = os.path.join(data_path, 'good_combined_airr.json')
 
         # Output data
         self.output_rep_good = os.path.join(data_path, 'output_rep_data.json')
@@ -49,11 +51,11 @@ class TestInferface(unittest.TestCase):
     # @unittest.skip('-> load(): skipped\n')
     def test_load_rearrangement(self):
         # Good data
-        result = airr.load_rearrangement(self.data_good)
+        result = airr.load_rearrangement(self.rearrangement_good)
         self.assertTupleEqual(result.shape, self.shape_good, 'load(): good data failed')
 
         # Bad data
-        result = airr.load_rearrangement(self.data_bad)
+        result = airr.load_rearrangement(self.rearrangement_bad)
         self.assertTupleEqual(result.shape, self.shape_bad, 'load(): bad data failed')
 
     # @unittest.skip('-> repertoire_template(): skipped\n')
@@ -69,50 +71,60 @@ class TestInferface(unittest.TestCase):
     def test_validate_rearrangement(self):
         # Good data
         try:
-            result = airr.validate_rearrangement(self.data_good)
+            result = airr.validate_rearrangement(self.rearrangement_good)
             self.assertTrue(result, 'validate(): good data failed')
         except:
             self.assertTrue(False, 'validate(): good data failed')
 
         # Bad data
         try:
-            result = airr.validate_rearrangement(self.data_bad)
+            result = airr.validate_rearrangement(self.rearrangement_bad)
             self.assertFalse(result, 'validate(): bad data failed')
         except Exception as inst:
             print(type(inst))
             raise inst
 
-    # @unittest.skip('-> load_airr_data(): skipped\n')
+    # @unittest.skip('-> read_airr(): skipped\n')
     def test_read_airr(self):
         # Good data
         try:
             data = airr.read_airr(self.rep_good, validate=True, debug=True)
         except:
-            self.assertTrue(False, 'load_airr_data(): good data failed')
+            self.fail('read_airr(): good data failed')
 
         # Bad data
-        try:
+        with self.assertRaises(ValidationError, msg="read_airr(): bad data passed validation"):
             data = airr.read_airr(self.rep_bad, validate=True, debug=True)
-            self.assertFalse(True, 'load_airr_data(): bad data failed')
-        except ValidationError:
-            pass
-        except Exception as inst:
-            print(type(inst))
-            raise inst
 
-    # @unittest.skip('-> validate_airr_data(): skipped\n')
+        # Combined yaml
+        try:
+            data_yaml = airr.read_airr(self.combined_yaml, validate=True, debug=True)
+        except:
+            self.fail('read_airr(): combined yaml failed')
+
+        # Combined yaml
+        try:
+            data_json = airr.read_airr(self.combined_json, validate=True, debug=True)
+        except:
+            self.fail('read_airr(): combined json failed')
+
+        # Check equality of yaml and json
+        self.assertDictEqual(data_yaml, data_json, msg="read_airr(): yaml and json imports are not equal")
+
+
+    # @unittest.skip('-> validate_airr(): skipped\n')
     def test_validate_airr(self):
         # Good data
         try:
             result = airr.validate_airr(self.rep_good, debug=True)
-            self.assertTrue(result, 'validate_airr_data(): good data failed')
+            self.assertTrue(result, 'validate_airr(): good data failed')
         except:
-            self.assertTrue(False, 'validate_airr_data(): good data failed')
+            self.assertTrue(False, 'validate_airr(): good data failed')
 
         # Bad data
         try:
             result = airr.validate_airr(self.rep_bad, debug=True)
-            self.assertFalse(result, 'validate_airr_data(): bad data failed')
+            self.assertFalse(result, 'validate_airr(): bad data failed')
         except ValidationError:
             pass
         except Exception as inst:
