9da846d69d44522bd4bdc5a295680c0e42aa6f30
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2024-05-21 11:18:10 -0700

    Updated versioneer from 0.18 to 0.29

--- lang/python/airr/__init__.py
+++ lang/python/airr/__init__.py
@@ -5,6 +5,5 @@ from airr.interface import *
 from airr.schema import ValidationError
 
 # versioneer-generated
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
