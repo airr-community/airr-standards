a494e51aec6225612856743a4b1caf8c03b7744c
Scott Christley <scott.christley@utsouthwestern.edu>
2024-10-16 21:55:54 -0500

    implement rearrangement write functions and tests

--- lang/js/tests/airr.test.js
+++ lang/js/tests/airr.test.js
@@ -27,24 +27,91 @@ var combined_yaml = path.resolve(data_path, 'good_combined_airr.yaml')
 var combined_json = path.resolve(data_path, 'good_combined_airr.json')
 
 // Output data
+var output_rearrangement_good = path.resolve(data_path, 'output_good_rearrangement.tsv')
+var output_rearrangement_good_gz = path.resolve(data_path, 'output_good_rearrangement.tsv.gz')
 //var output_rep = os.path.join(data_path, 'output_rep.json')
 var output_good = path.resolve(data_path, 'output_data.json')
 var output_good_yaml = path.resolve(data_path, 'output_data.yaml')
 //var output_blank = os.path.join(data_path, 'output_blank.json')
 
 test('load schema', async () => {
-  const schema = await airr.load_schema();
-  expect(schema).not.toBeNull();
+    const schema = await airr.load_schema();
+    expect(schema).not.toBeNull();
 });
 
 test('load good airr yaml', () => {
-  const data = airr.read_airr(rep_good, true);
-  expect(data).not.toBeNull();
+    const data = airr.read_airr(rep_good, true);
+    expect(data).not.toBeNull();
 });
 
-test('load good rearrangement tsv', () => {
-  const data = airr.load_rearrangement(rearrangement_good, true);
-  expect(data).not.toBeNull();
+test('load good rearrangement tsv', async () => {
+    const data = await airr.load_rearrangement(rearrangement_good, true);
+    expect(data).not.toBeNull();
+});
+
+test('write good AIRR Rearrangement TSV', async () => {
+    const data = await airr.load_rearrangement(rearrangement_good, true);
+    expect(data).not.toBeNull();
+
+    var idx = 0;
+    var row_callback = function(fields) {
+        if (idx >= data.length) return null;
+        else return data[idx++];
+    };
+
+    await airr.create_rearrangement(output_rearrangement_good, row_callback);
+
+    const new_data = await airr.load_rearrangement(output_rearrangement_good, true);
+    expect(new_data).not.toBeNull();
+});
+
+test('dump good AIRR Rearrangement TSV', async () => {
+    const data = await airr.load_rearrangement(rearrangement_good, true);
+    expect(data).not.toBeNull();
+
+    await airr.dump_rearrangement(data, output_rearrangement_good);
+
+    const new_data = await airr.load_rearrangement(output_rearrangement_good, true);
+    expect(new_data).not.toBeNull();
+});
+
+test('derive good AIRR Rearrangement TSV', async () => {
+    const data = await airr.load_rearrangement(rearrangement_good, true);
+    expect(data).not.toBeNull();
+
+    var idx = 0;
+    var row_callback = function(fields) {
+        if (idx >= data.length) return null;
+        else return data[idx++];
+    };
+
+    await airr.derive_rearrangement(output_rearrangement_good, rearrangement_good, row_callback);
+
+    const new_data = await airr.load_rearrangement(output_rearrangement_good, true);
+    expect(new_data).not.toBeNull();
+});
+
+test('validate good rearrangement tsv', async () => {
+    let isValid = true;
+    const data = await airr.validate_rearrangement(rearrangement_good, true)
+        .catch(function(error) { console.error(error); isValid = false; });
+    expect(data).not.toBeNull();
+    expect(isValid).toBe(true);
+});
+
+test('validate bad rearrangement tsv', async () => {
+    let isValid = true;
+    const data = await airr.validate_rearrangement(rearrangement_bad, true)
+        .catch(function(error) { console.error(error); isValid = false; });
+    expect(isValid).toBe(false);
+});
+
+test('merge good AIRR Rearrangement TSV', async () => {
+    var in_files = [ rearrangement_good, rearrangement_good, rearrangement_good];
+    await airr.merge_rearrangement(output_rearrangement_good, in_files, false, true);
+
+    const new_data = await airr.load_rearrangement(output_rearrangement_good, true);
+    expect(new_data).not.toBeNull();
 });
 
 test('write good AIRR DataFile', () => {
